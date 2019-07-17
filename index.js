e005d824fe99e95b30d282f015a4d5947591c2ec
king-ho <kiho@greenpeace>
2019-07-17 17:07:28 +0200

    generated files on 2019-07-17 17:07:28

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ let networks = {
          interfaces.push(r)
          for(key in networks.pingtests){
            if(networks.pingtests[key].ip.includes(r.tm)){
-             console("found "+r.sr+" matches "+networks.pingtests[key].ip)
+             console.log("found "+r.sr+" matches "+networks.pingtests[key].ip)
              networks.pingtests[key].src=r.sr
            }
          }
